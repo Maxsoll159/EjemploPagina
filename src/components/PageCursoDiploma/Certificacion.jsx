d3abe03cec0e674c5b0bdbe3d0dab3d9cfa4348a
Martin <mar_1905@hotmail.es>
2022-09-30 10:21:44 -0500

    Cambios Certificados

--- src/components/PageCursoDiploma/Certificacion.jsx
+++ src/components/PageCursoDiploma/Certificacion.jsx
@@ -1,11 +1,11 @@
 import { Container, Row, Col } from "react-bootstrap";
-export const Certificacion = ({tipo}) => {
+export const Certificacion = ({ tipo }) => {
     return (
         <div className="color-certiticado-diplomado mt-5" id="certificado">
             <Container>
                 <Row>
                     <Col xl={8} lg={8} md={12} sm={12}>
-                        <h3 className="fw-bold mt-5 text-center-res">Doble Certificación Universitaria</h3>
+                        <h3 className="fw-bold mt-5 text-center-res">{tipo === "Diploma" ? (<>Doble</>) : (<></>)} Certificación Universitaria</h3>
                         <div className="bg-white rounded p-5 mt-4 shadow">
                             <p className="fw-bolder">👉 Válido para postular a convocatorias Públicas y Privadas</p>
                             <p className="m-0 fw-bolder w-75 w-100res text-start">Los Certificados son válidos y aceptados por instituciones públicas, de acuerdo a la Normativa Nº141-2016-SERVIR-PE.</p>
@@ -14,15 +14,17 @@ export const Certificacion = ({tipo}) => {
                                     <img src="https://nuevapagina.s3.amazonaws.com/vistoDiploma.webp" alt="" />
                                     <p className="m-0 fw-bold text-start">Certificado por Curso</p>
                                 </div>
-                                <div className="d-flex align-items-center gap-3 gap-response-0">
-                                    <img src="https://nuevapagina.s3.amazonaws.com/vistoDiploma.webp" alt="" />
-                                    <p className="m-0 fw-bold text-start">Diploma de Especialización</p>
-                                </div>
+                                {
+                                    tipo === "Diploma" ? (<div className="d-flex align-items-center gap-3 gap-response-0">
+                                        <img src="https://nuevapagina.s3.amazonaws.com/vistoDiploma.webp" alt="" />
+                                        <p className="m-0 fw-bold text-start">Diploma de Especialización</p>
+                                    </div>) : (<></>)
+                                }
                             </div>
                         </div>
                         {
-                            tipo === "Diploma" ? (<img src="https://nuevapagina.s3.amazonaws.com/Diploma.webp" alt="" className="p-4 mb-4 mt-4 img-fluid" />) : 
-                            (<img src="https://nuevapagina.s3.amazonaws.com/Certificado.webp" alt="" className="p-4 mb-4 mt-4 img-fluid" />)
+                            tipo === "Diploma" ? (<img src="https://nuevapagina.s3.amazonaws.com/Diploma.webp" alt="" className="p-4 mb-4 mt-4 img-fluid" />) :
+                                (<img src="https://nuevapagina.s3.amazonaws.com/Certificado.webp" alt="" className="p-4 mb-4 mt-4 img-fluid" />)
                         }
                     </Col>
                 </Row>
