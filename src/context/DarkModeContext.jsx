61cfdb49dc3fe34b575bac4c895d54d9085639d8
Martin <mar_1905@hotmail.es>
2022-10-01 11:38:57 -0500

    Login

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -19,7 +19,7 @@ export const DarkModeProvider = ({ children }) => {
             const data = new FormData()
             data.append('token', token)
             valirdarUsuario(data).then((resp) =>{
-                if(!resp){
+                if(resp !== true){
                     setUsuarioLogin(resp)
                     localStorage.setItem("usuarioDesarrollo", JSON.stringify(resp))
                 }else{
