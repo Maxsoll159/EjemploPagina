6b5e752766f5c7eebbd81d8f9a069bc372509826
Martin <mar_1905@hotmail.es>
2022-10-10 09:33:21 -0500

    Arrglo aletar

--- src/components/Navbar/AlertaSeminario.jsx
+++ src/components/Navbar/AlertaSeminario.jsx
@@ -1,22 +1,27 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 import useContador from "../../hooks/useContador"
 
 
 export const AlertaSeminario = (prop) => {
+    let navigate = useNavigate()
     const [fecha, setFecha] = useState("")
 
     useEffect(() => {
         if (prop[0] !== undefined) {
             setFecha(prop[0].fecha)
         }
+        console.log(prop)
     }, [prop])
-    console.log("que honda  ", fecha)
 
     let fechaparseada
     const [timerDays, timerHours, timerMinutes, timerSeconds] = useContador(
         fechaparseada = String(new Date(fecha).toUTCString()).split(" ")
     )
+    const irSeminario = (title) =>{
+        navigate(`seminariosInfo/${title}`, {replace: false})
+    }
 
     return (
         <>
@@ -28,10 +33,10 @@ export const AlertaSeminario = (prop) => {
                             <button className="btn text-danger fw-bolder bg-white"><a href="https://aula.desarrolloglobal.pe/prox-seminario/sistemas-administrativos-del-estado-10081" className="text-danger text-decoration-none"> <img src="/img/icons/LiveSeminario.webp" alt="" /> Ver Seminario</a></button>
                         </div>) : (
                         <div style={{ background: "#2C3C67" }}>
-                            <div className="d-flex align-items-center justify-content-center">
+                            <div className="d-flex align-items-center justify-content-center flex-wrap py-3 py-xl-0 py-lg-0 flex-md-nowrap w-100">
                                 <p className="fw-bolder text-center m-0 text-white">🚀 Nuestro Proximo seminario gratuito inicia en: </p>
-                                <div className="d-flex gap-3 px-5 py-1 align-items-center">
-                                    <div className="d-flex gap-3 d-lg-none d-xl-flex my-2">
+                                <div className="d-flex gap-3 px-lg-5 px-xl-5 px-sm-5 py-1 align-items-center">
+                                    <div className="d-flex gap-3 d-xl-flex my-2">
                                         <div className="bg-danger rounded px-3">
                                             <p className="m-0 fw-bolder text-center text-white fs-3">{timerDays}</p>
                                             <p className="m-0 mb-1 fw-bolder text-center text-white font-size-11">Dias</p>
@@ -50,7 +55,7 @@ export const AlertaSeminario = (prop) => {
                                         </div>
                                     </div>
                                 </div>
-                                <button className="btn btn-success fw-bolder">Registrate Aquí</button>
+                                <button className="btn btn-success fw-bolder" onClick={()=>irSeminario(prop[0].etiqueta)}>Registrate Aquí</button>
                             </div>
                         </div>)
                 ) : (<></>)
