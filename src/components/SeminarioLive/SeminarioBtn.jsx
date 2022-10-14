2ad7ee9a3b93a384aa6ed4f581125adc37c605b9
Martin <mar_1905@hotmail.es>
2022-10-14 18:33:34 -0500

    Chat En Proceso

---
+++ src/components/SeminarioLive/SeminarioBtn.jsx
@@ -0,0 +1,127 @@
+import { useState } from "react"
+import { Col } from "react-bootstrap"
+import { ModalLive } from "./ModalLive"
+import { io } from 'socket.io-client'
+import { useEffect } from "react"
+export const SeminarioBtn = (seminarios) => {
+
+    const [{ chat, detalle, promo }, setPartes] = useState({
+        chat: true,
+        detalle: false,
+        promo: false
+    })
+    const [idUsuario, setIdUsuario] = useState(2005)
+
+    /*Configuracion de Socket*/
+    const [mensajes, setMensajes] = useState([])
+    const [mensaje, setMensaje] = useState('')
+    const socket = io('http://45.33.81.115:3035')
+    useEffect(() => {
+        socket.on('connect', () => {
+            const user = { id: 2005, name: 'Martin' }
+            socket.emit('conectado',
+                1001, //id seminario
+                user
+            )
+        })
+    }, [socket])
+
+    useEffect(() => {
+        if (mensaje !== '') {
+            socket.emit('enviar_mensaje', {
+                room: 1001, //id seminario
+                user: 2005,
+                content: mensaje
+            })
+        }
+    }, [mensaje])
+
+    socket.on('mostrar_mensajes', mensajes => {
+        setMensajes(mensajes)
+    })
+
+    socket.on('mostrar_usuarios', usuarios => {
+        
+    })
+
+    const enviar = (e) => {
+        e.preventDefault()
+        setMensaje(e.target.mensaje.value)
+    }
+    console.log("oe nad",mensajes)
+    return (
+        <Col xl={3} sm={12} className={`p-0 color-live`}>
+            <div>
+                <div className='d-flex mt-3'>
+                    <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${chat === true ? "activate" : ""}`} onClick={() =>
+                        setPartes({
+                            chat: true,
+                            detalle: false,
+                            promo: false
+                        })
+                    }><img loading='lazy' src="/img/icons/chat.png" alt="" className='d-block mx-auto' /><p>Chat en Vivo</p></button>
+                    <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${detalle === true ? "activate" : ""}`} onClick={() => setPartes({
+                        chat: false,
+                        detalle: true,
+                        promo: false
+                    })
+                    }><img loading='lazy' src="/img/icons/detalles.png" alt="" className='d-block mx-auto' /><p>Detalles</p></button>
+                    <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${promo === true ? "activate" : ""}`} onClick={() => setPartes({
+                        chat: false,
+                        detalle: false,
+                        promo: true
+                    })}><img loading='lazy' src="/img/icons/promo.png" alt="" className='d-block mx-auto' /><p>Promocion</p></button>
+                </div>
+            </div>
+
+            <div className='text-white'>
+                {
+                    chat === true ? (
+                        <div className='res resTablet'>
+                            <div className="mx-3 mb-3">
+                                {
+                                    mensajes !== [] ? (
+                                        mensajes.map((men) => (
+                                            <div>
+                                                <img src="" alt="" />
+                                                <div className={`p-3 rounded ${idUsuario !== men.user ? "color-chat1" : "color-chat2"}`}>
+                                                    <p className="m-0 text-white fw-bolder">{men.name}</p>
+                                                    <p className="m-0">{men.message}</p>
+                                                </div>
+                                            </div>
+                                        ))
+                                    ) : (<>No hay mensajkes...</>)
+                                }
+                            </div>
+                            <form action="" onSubmit={enviar} className="w-100">
+                                <input type="text" id="mensaje" className="rounded p-3 w-75" placeholder="Escribe tu comentario o pregunta..." />
+                                <button className="btn w-25 btn-primary h-100">Enviar</button>
+                            </form>
+                        </div>
+                    ) : detalle === true ? (
+                        <div className='p-4 res resTablet'>
+                            <h4 style={{ color: "#3C86F4" }} className="fw-bold">Tema del Seminario</h4>
+                            <p className={`fw-bolder fs-5 text-white`}>{seminarios.titulo}</p>
+                            <h4 style={{ color: "#3C86F4" }} className="mt-3 fw-bolder">¿Quien es el docente?</h4>
+                            <div className='text-dark p-2 rounded d-flex align-items-center gap-3'>
+                                <img loading='lazy' src={seminarios.profesor.perfil} alt="" width={100} className="rounded-circle" />
+                                <h3 className={`text-white`}>{seminarios.profesor.nombre}</h3>
+                            </div>
+                            <p className={`mt-3 text-justify text-white`}> {seminarios.profesor.descripcion}</p>
+                        </div>
+                    ) : (
+                        <div className='p-4 res resTablet'>
+                            <img loading='lazy' src={seminarios.banner.promocion} alt="" className='rounded' style={{ width: "100%" }} />
+                            <div>
+                                <h5 className='text-center mt-3'>Estamos en Linea 😃</h5>
+                                <a className='btn btn-success w-100 p-2 fs-4 fw-bold' href={`https://api.whatsapp.com/send?phone=51${seminarios.asesor[0].telefono}&text=Hola,%20solicito%20información%20del%20%20diploma:${seminarios.titulo},%20mi%20correo%20es:`}><i class="fa fa-whatsapp" aria-hidden="true"></i> {seminarios.asesor[0] === undefined ? (983495578) : (seminarios.asesor[0].telefono)}</a>
+                                <ModalLive imagen={seminarios.banner.promocion} titulo={seminarios.titulo} fecha={seminarios.fecha} />
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+
+        </Col>
+    )
+}
--- src/pages/Seminarios.jsx
+++ src/pages/Seminarios.jsx
@@ -9,15 +9,14 @@ import Plyr from "plyr";
 import "../../node_modules/plyr/dist/plyr.css";
 import { Helmet } from 'react-helmet';
 import ReactHlsPlayer from 'react-hls-player/dist';
+import { VideoSeminario } from '../components/SeminarioLive/VideoSeminario';
+import { SeminarioBtn } from '../components/SeminarioLive/SeminarioBtn';
+
 export const Seminarios = () => {
     let { id } = useParams();
     const [seminarios, setSeminarios] = useState([])
     const { isdark } = useContext(UserContext)
-    const [{ chat, detalle, promo }, setPartes] = useState({
-        chat: true,
-        detalle: false,
-        promo: false
-    });
+
     useEffect(() => {
         seminarioId(id).then((resp) => setSeminarios(resp))
     }, [id])
@@ -38,7 +37,7 @@ export const Seminarios = () => {
             };
         }
     }, 10)
-    console.log(seminarios)
+
     return (
         <>
             <Helmet>
@@ -68,150 +67,62 @@ export const Seminarios = () => {
                 </Helmet>
             </Helmet>
 
-            {(seminarios === [] || seminarios === undefined) ? (<h1>Cargando</h1>)
-                : (<Container fluid>
-                    <Row style={{ height: "calc(100vh - 76.72px)" }}>
-                        <Col xl={9} sm={12} className={`p-0 ${!isdark ? "color-live-litgth" : "color"}`}>
-                            {
-                                seminarios.tipo === "PROXIMO" ? (
-                                    <ReactHlsPlayer
-                                        src={`https://antmediaserver.desarrolloglobal.pe:5443/LiveApp/streams/${seminarios.codigo_envivo}.m3u8`}
-                                        autoPlay={true}
-                                        controls={true}
-                                        width="100%"
-                                        height="73%"
-                                        className='h-100res'
-                                    />
-                                ) : (
-                                    <video loading="lazy" id="vdplayer" ratio="14:6" className='h-100res'></video>
-                                )
-                            }
-                            <div className='pt-2 ps-4 pe-4 ocultar'>
-                                <div className='d-flex align-items-center justify-content-between mt-2'>
-                                    <div className='caja-camara rounded'>
-                                        <img loading='lazy' src="/img/icons/camara.png" alt="" className='' />
-                                    </div>
-                                    <h5 className={`w-50 p-0 m-0 fw-bolder ${!isdark ? "text-dark" : "text-white"}`}>{seminarios.titulo}</h5>
-                                    <div className='d-flex align-items-center'>
-                                        <div className='bg-white' style={{ width: "2px", height: "20px" }}></div>
-                                        {
-                                            !isdark ? (<img loading='lazy' src="/img/icons/user.webp" alt="" className='ms-3' />)
-                                                : (<img loading='lazy' src="/img/iconsDarkMode/userWhite.webp" alt="" className='ms-3' />)
-                                        }
-                                        <p className={`p-0 m-0 ms-3 fw-bold ${!isdark ? "text-dark" : "text-white"}`}>230</p>
-                                    </div>
-                                    <div className={`d-flex align-items-center pt-3 py-3 ps-4 px-4 rounded ${!isdark ? "text-primary bg-white shadow" : "text-white color-fecha "}`}>
-                                        {
-                                            !isdark ? (<img loading='lazy' src="/img/iconsDarkMode/calendarWhite.webp" alt="" />)
-                                                : (<img loading='lazy' src="/img/icons/calendar.png" alt="" />)
-                                        }
-                                        <p className='p-0 m-0 ps-3 fw-bold'>{(seminarios.fecha !== undefined) ? ((seminarios.fecha).substring(8, 10)) : ""} de {(seminarios.fecha !== undefined) ? (parsearFecha(seminarios.fecha)) : ""} | {(seminarios.fecha !== undefined) ? (parsearHora(seminarios.hora)) : ""}</p>
-                                    </div>
-                                    <div>
-                                        <button className='bg-white p-3 rounded text-primary fw-bold shadow' style={{ border: "none" }}>Compartir <img loading='lazy' src="/img/icons/compartir.png" alt="" /></button>
-                                    </div>
+            <Container fluid>
+                <Row style={{ height: "calc(100vh - 76.72px)" }}>
+                    <Col xl={9} sm={12} className={`p-0 ${!isdark ? "color-live-litgth" : "color"}`}>
 
+                        {
+                            seminarios.tipo === "PROXIMO" ? (
+                                <ReactHlsPlayer
+                                    src={`https://antmediaserver.desarrolloglobal.pe:5443/LiveApp/streams/${seminarios.codigo_envivo}.m3u8`}
+                                    autoPlay={true}
+                                    controls={true}
+                                    width="100%"
+                                    height="73%"
+                                    className='h-100res'
+                                />
+                            ) : (
+                                <video loading="lazy" id="vdplayer" ratio="14:6" className='h-100res'></video>
+                            )
+                        }
+                        <div className='pt-2 ps-4 pe-4 ocultar'>
+                            <div className='d-flex align-items-center justify-content-between mt-2'>
+                                <div className='caja-camara rounded'>
+                                    <img loading='lazy' src="/img/icons/camara.png" alt="" className='' />
                                 </div>
-                                <div style={{ borderTop: "3px solid #14206B" }} className="mt-3">
-                                    <h5 className={`mt-2 ${!isdark ? "color-unete-ligth" : "text-white"}`}>Unete a nuestros grupos y sigue los seminarios</h5>
-                                    <div className='mt-3'>
-                                        <a className='btn btn-success fw-bold'>Grupo Whatsapp</a>
-                                        <button className='btn btn-primary fw-bold ms-3'>Grupo Telegram</button>
-                                    </div>
+                                <h5 className={`w-50 p-0 m-0 fw-bolder ${!isdark ? "text-dark" : "text-white"}`}>{seminarios.titulo}</h5>
+                                <div className='d-flex align-items-center'>
+                                    <div className='bg-white' style={{ width: "2px", height: "20px" }}></div>
+                                    {
+                                        !isdark ? (<img loading='lazy' src="/img/icons/user.webp" alt="" className='ms-3' />)
+                                            : (<img loading='lazy' src="/img/iconsDarkMode/userWhite.webp" alt="" className='ms-3' />)
+                                    }
+                                    <p className={`p-0 m-0 ms-3 fw-bold ${!isdark ? "text-dark" : "text-white"}`}>230</p>
+                                </div>
+                                <div className={`d-flex align-items-center pt-3 py-3 ps-4 px-4 rounded ${!isdark ? "text-primary bg-white shadow" : "text-white color-fecha "}`}>
+                                    {
+                                        !isdark ? (<img loading='lazy' src="/img/iconsDarkMode/calendarWhite.webp" alt="" />)
+                                            : (<img loading='lazy' src="/img/icons/calendar.png" alt="" />)
+                                    }
+                                    <p className='p-0 m-0 ps-3 fw-bold'>{(seminarios.fecha !== undefined) ? ((seminarios.fecha).substring(8, 10)) : ""} de {(seminarios.fecha !== undefined) ? (parsearFecha(seminarios.fecha)) : ""} | {(seminarios.fecha !== undefined) ? (parsearHora(seminarios.hora)) : ""}</p>
+                                </div>
+                                <div>
+                                    <button className='bg-white p-3 rounded text-primary fw-bold shadow' style={{ border: "none" }}>Compartir <img loading='lazy' src="/img/icons/compartir.png" alt="" /></button>
+                                </div>
+
+                            </div>
+                            <div style={{ borderTop: "3px solid #14206B" }} className="mt-3">
+                                <h5 className={`mt-2 ${!isdark ? "color-unete-ligth" : "text-white"}`}>Unete a nuestros grupos y sigue los seminarios</h5>
+                                <div className='mt-3'>
+                                    <a className='btn btn-success fw-bold'>Grupo Whatsapp</a>
+                                    <button className='btn btn-primary fw-bold ms-3'>Grupo Telegram</button>
                                 </div>
                             </div>
-                        </Col>
-                        <Col xl={3} sm={12} className={`p-0 ${!isdark ? "bg-white shadow" : "color-live"}`}>
-                            <div>
-                                {
-                                    !isdark ? (<div className='d-flex mt-3'>
-                                        <button className={`caja-live-ligth ms-1 p-3 text-white fw-bolder ${chat === true ? "activate-caja-ligth" : ""}`} onClick={() =>
-                                            setPartes({
-                                                chat: true,
-                                                detalle: false,
-                                                promo: false
-                                            })
-                                        }>
-                                            {
-                                                chat === true ? (<img loading='lazy' src="/img/iconsDarkMode/chatActivo.svg" alt="" className='d-block mx-auto' />)
-                                                    : (<img loading='lazy' src="/img/iconsDarkMode/chat.svg" alt="" className='d-block mx-auto' />)
-                                            }
-                                            <p>Chat en Vivo</p></button>
-
-                                        <button className={`caja-live-ligth ms-1 p-3 text-white fw-bolder ${detalle === true ? "activate-caja-ligth" : ""}`} onClick={() => setPartes({
-                                            chat: false,
-                                            detalle: true,
-                                            promo: false
-                                        })
-                                        }>{detalle === true ? (<img loading='lazy' src="/img/iconsDarkMode/LibreriaWhite.svg" alt="" className='d-block mx-auto' />) :
-                                            (<img loading='lazy' src="/img/iconsDarkMode/Libreria.svg" alt="" className='d-block mx-auto' />)
-                                            }<p>Detalles</p></button>
-
-                                        <button className={`caja-live-ligth ms-1 p-3 text-white fw-bolder ${promo === true ? "activate-caja-ligth" : ""}`} onClick={() => setPartes({
-                                            chat: false,
-                                            detalle: false,
-                                            promo: true
-                                        })}>{
-                                                promo === true ? (<img loading='lazy' src="/img/iconsDarkMode/RegaloWhite.svg" alt="" className='d-block mx-auto' />)
-                                                    : (<img loading='lazy' src="/img/iconsDarkMode/Regalo.svg" alt="" className='d-block mx-auto' />)
-                                            }<p>Promocion</p></button>
-                                    </div>)
-                                        :
-                                        (<div className='d-flex mt-3'>
-                                            <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${chat === true ? "activate" : ""}`} onClick={() =>
-                                                setPartes({
-                                                    chat: true,
-                                                    detalle: false,
-                                                    promo: false
-                                                })
-                                            }><img loading='lazy' src="/img/icons/chat.png" alt="" className='d-block mx-auto' /><p>Chat en Vivo</p></button>
-                                            <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${detalle === true ? "activate" : ""}`} onClick={() => setPartes({
-                                                chat: false,
-                                                detalle: true,
-                                                promo: false
-                                            })
-                                            }><img loading='lazy' src="/img/icons/detalles.png" alt="" className='d-block mx-auto' /><p>Detalles</p></button>
-                                            <button className={`cajas-live ms-1 p-3 text-white fw-bolder ${promo === true ? "activate" : ""}`} onClick={() => setPartes({
-                                                chat: false,
-                                                detalle: false,
-                                                promo: true
-                                            })}><img loading='lazy' src="/img/icons/promo.png" alt="" className='d-block mx-auto' /><p>Promocion</p></button>
-                                        </div>)
-                                }
-                            </div>
-                            <div className='text-white'>
-                                {
-                                    chat === true ? (
-                                        <div className='res resTablet'>
-                                            <h1>Chat</h1>
-                                        </div>
-                                    ) : detalle === true ? (
-                                        <div className='p-4 res resTablet'>
-                                            <h4 style={{ color: "#3C86F4" }} className="fw-bold">Tema del Seminario</h4>
-                                            <p className={`fw-bold fs-5 ${!isdark ? "text-dark" : "text-white"}`}>{seminarios.titulo}</p>
-                                            <h4 style={{ color: "#3C86F4" }} className="mt-3 fw-bolder">¿Quien es el docente?</h4>
-                                            <div className='text-dark p-2 rounded d-flex align-items-center gap-3'>
-                                                <img loading='lazy' src={seminarios.profesor.perfil} alt="" width={100} className="rounded-circle" />
-                                                <h3 className={`${isdark ? "text-white" : "text-dark"}`}>{seminarios.profesor.nombre}</h3>
-                                            </div>
-                                            <p className={`mt-3 text-justify ${!isdark ? "text-dark" : "text-white"}`}> {seminarios.profesor.descripcion}</p>
-                                        </div>
-                                    ) : (
-                                        <div className='p-4 res resTablet'>
-                                            <img loading='lazy' src={seminarios.banner.promocion} alt="" className='rounded' style={{ width: "100%" }} />
-                                            <div>
-                                                <h5 className='text-center mt-3'>Estamos en Linea 😃</h5>
-                                                <a className='btn btn-success w-100 p-2 fs-4 fw-bold' href={`https://api.whatsapp.com/send?phone=51${seminarios.asesor[0].telefono}&text=Hola,%20solicito%20información%20del%20%20diploma:${seminarios.titulo},%20mi%20correo%20es:`}><i class="fa fa-whatsapp" aria-hidden="true"></i> {seminarios.asesor[0] === undefined ? (983495578) : (seminarios.asesor[0].telefono)}</a>
-                                                <ModalLive imagen={seminarios.banner.promocion} titulo={seminarios.titulo} fecha={seminarios.fecha} />
-                                            </div>
-                                        </div>
-                                    )
-                                }
-
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>)}
+                        </div>
+                    </Col>
+                    <SeminarioBtn {...seminarios} />
+                </Row>
+            </Container>
         </>
 
     )
