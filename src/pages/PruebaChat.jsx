3cf084ff45ed4893f1656b87eab3d4f2a0aa3e4b
Martin <mar_1905@hotmail.es>
2022-10-21 12:07:06 -0500

    Prueba Socket

--- src/pages/PruebaChat.jsx
+++ src/pages/PruebaChat.jsx
@@ -6,7 +6,8 @@ import io from 'socket.io-client';
 export const PruebaChat = () => {
     const socket = io('https://desarrolloglobal.pe:8443')
 
-
+    const [mensaje, setMensaje] = useState("")
+    const [mensajes, setMensajes] = useState([])
 
     useEffect(() => {
         socket.on('connect', () => {
@@ -21,25 +22,49 @@ export const PruebaChat = () => {
         })
     }, [])
 
+    const enviarMensaje = (e) => {
+        e.preventDefault();
+        socket.emit("enviar_mensaje", 1001, mensaje);
+        setMensaje("");
+    };
 
-    const enviarMensaje = e => {
-        e.preventDefault()
-        let mensajer = e.target.mensaje.value
-        socket.emit('enviar_mensaje', 1001, mensajer)
-    }
 
-    socket.on('mostrar_mensaje', data => {
-        console.info(data)
-    })
+    useEffect(() => {
+        socket.on('mostrar_mensaje', data => {
+            setMensajes([...data])
+        })
+    }, [mensaje])
 
+
+    useEffect(() => {
+        socket.on('mostrar_total_mensajes', data => {
+            setMensajes(data)
+        })
+    }, [])
 
 
 
     return (
-        <form onSubmit={enviarMensaje}>
-            <input type='text' placeholder='escriba un mensaje' id="mensaje" />
-            <button type='submit'>Enviar</button>
-        </form>
+        <>
+            <div>
+                {
+                    mensajes !== undefined ? (
+                        mensajes.map((men, index)=>(
+                            <div key={index}>
+                                <p>{men.nombre}</p>
+                                <p>{men.contenido}</p>
+                            </div>
+                        ))
+                    ): (<>Error</>)                
+                }
+            </div>
+
+            <form onSubmit={enviarMensaje}>
+                <input type='text' placeholder='escriba un mensaje' value={mensaje} onChange={e => setMensaje(e.target.value)} />
+                <button type='submit'>Enviar</button>
+            </form>
+        </>
+
 
     )
 
