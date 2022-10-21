ef9907c8442cc20a066d644bbb0204005b042838
Martin <mar_1905@hotmail.es>
2022-10-21 10:22:48 -0500

    SocketDavid

--- src/pages/PruebaChat.jsx
+++ src/pages/PruebaChat.jsx
@@ -4,52 +4,48 @@ import io from 'socket.io-client';
 
 
 export const PruebaChat = () => {
-    let Socket = io("https://desarrolloglobal.pe:8443")
+    const socket = io('https://desarrolloglobal.pe:8443')
 
-    const [mensajes, setMensajes] = useState([])
-    const [datos, setDatos] = useState(
-        { id: 3, nombre: "Chupet", avatar: "wwww.hola.com" }
-    )
+
+
     useEffect(() => {
-        Socket.on('connect', () => {
-            Socket.emit('conectar',
-                1001, {id: 8, nombre: "Martin", avatar: "google.com"}
+        socket.on('connect', () => {
+            console.log('==> conectado')
+            socket.emit('conectar',
+                1001, {
+                id: 7,
+                nombre: 'Mrtin',
+                avatar: 'https://www.google.es'
+            }
             )
         })
     }, [])
 
 
-    useEffect(() => {
-        Socket.on('mostrar_total_mensajes', data => (
-            setMensajes([...data])
-        ))
-    }, [mensajes])
-
-    const submit = (e) => {
+    const enviarMensaje = e => {
         e.preventDefault()
-        let mensaje = e.target.mensaje.value
-        Socket.emit('enviar_mensaje', 1001, mensaje)
+        socket.emit('enviar_mensaje', 1001, 'ESTO ES UN MENSAJE')
     }
-    console.log("asdasd", mensajes)
+
+    socket.on('mostrar_mensaje', data => {
+        console.info(data)
+    })
+
+
+
+    socket.on('mostrar_total_mensajes', data => {
+        console.info(data)
+    })
+
+
+
     return (
-        <div>
-            <div>
-                {mensajes.map((e, index) => (
-                    <div key={index}>
-                        <img src={e.avatar} alt="" width={50} />
-                        <div>{e.nombre}</div>
-                        <div>{e.mensaje}</div>
-                    </div>
-                ))}
-            </div>
-
-
-            <form onSubmit={submit}>
-                <label htmlFor="">Escriba su mensaje</label>
-                <textarea name="" cols="30" rows="10" id="mensaje"></textarea>
-                <button>Enviar</button>
-            </form>
-        </div>
+        <form onSubmit={enviarMensaje}>
+            <input type='text' placeholder='escriba un mensaje' />
+            <button type='submit'>Enviar</button>
+        </form>
 
     )
+
 }
+
