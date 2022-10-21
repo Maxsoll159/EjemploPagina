4e3c09e37ff1b4de49e234b7d3d8ea2de8eaad9f
Martin <mar_1905@hotmail.es>
2022-10-21 10:05:51 -0500

    Scoket David

--- src/pages/PruebaChat.jsx
+++ src/pages/PruebaChat.jsx
@@ -3,34 +3,37 @@ import io from 'socket.io-client';
 
 
 
-export const PruebaChat = () =>{
-    let Socket = io("https://desarrolloglobal.pe:8443/")
-    const [mensaje, setMensaje] = useState("")
+export const PruebaChat = () => {
+    let Socket = io("https://desarrolloglobal.pe:8443")
+
     const [mensajes, setMensajes] = useState([])
+    const [datos, setDatos] = useState(
+        { id: 3, nombre: "Chupet", avatar: "wwww.hola.com" }
+    )
+    useEffect(() => {
+        Socket.on('connect', () => {
+            Socket.emit('conectar',
+                1001,
+                8,
+                'MARTIN',
+                'https://www.google.es'
+            )
+        })
+    }, [])
+
 
     useEffect(() => {
-        Socket.emit("conectado", "Juan");
-    }, []);
-    console.log(Socket)
-
-    useEffect(() => {
-        Socket.on("mensajes", (mensaje) => {
-            setMensajes([...mensajes, mensaje]);
-        });
-
-        return () => {
-            Socket.off();
-        };
-    }, [mensajes]);
+        Socket.on('mostrar_total_mensajes', data => (
+            setMensajes([...data])
+        ))
+    }, [mensajes])
 
     const submit = (e) => {
-        e.preventDefault();
-        Socket.emit("mensaje", "Juan", mensaje, "sd");
-        setMensaje("");
-    };
-
-
-    console.log(mensajes)
+        e.preventDefault()
+        let mensaje = e.target.mensaje.value
+        Socket.emit('enviar_mensaje', 1001, mensaje)
+    }
+    console.log("asdasd", mensajes)
     return (
         <div>
             <div>
@@ -46,7 +49,7 @@ export const PruebaChat = () =>{
 
             <form onSubmit={submit}>
                 <label htmlFor="">Escriba su mensaje</label>
-                <textarea name="" id="" cols="30" rows="10" value={mensaje} onChange={e => setMensaje(e.target.value)}></textarea>
+                <textarea name="" cols="30" rows="10" id="mensaje"></textarea>
                 <button>Enviar</button>
             </form>
         </div>
