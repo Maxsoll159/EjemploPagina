fe32ff4f33094a12d4fe5a0b3c113547fb2a1ae6
Martin <mar_1905@hotmail.es>
2022-10-22 10:41:05 -0500

    Socket Form

--- src/components/SeminarioLive/SeminarioChat.jsx
+++ src/components/SeminarioLive/SeminarioChat.jsx
@@ -21,33 +21,35 @@ export const SeminarioChat = ({ id, nombre, idSeminario, avatar }) => {
 
     const divRef = useRef(null);
     useEffect(() => {
-      divRef.current.scrollIntoView({ behavior: "smooth" });
+        divRef.current.scrollIntoView({ behavior: "smooth" });
     });
     /*Configuracion de Socket*/
     const mensajeRef = useRef()
 
     return (
-        <div className="mx-3 mb-3" style={{ height: "580px", overflow: "auto" }} >
-            {
-                mensajesChat !== [] ? (
-                    mensajesChat.map((men, index) => (
-                        <div className="d-flex w-100 gap-3 align-items-center" key={index}>
-                            <img src={men.avatar} alt="" width={50} height={50} className="rounded rounded-circle" />
-                            <div className={`p-3 rounded w-100 mt-3 ${men.usuario === id ? "color-chat1" : "color-chat2"}`}>
-                                <p className="m-0 text-white fw-bolder">{men.nombre}</p>
-                                <p className="m-0">{men.contenido}</p>
+        <>
+            <div className="mx-3 mb-3" style={{ height: "580px", overflow: "auto" }} >
+                {
+                    mensajesChat !== [] ? (
+                        mensajesChat.map((men, index) => (
+                            <div className="d-flex w-100 gap-3 align-items-center" key={index}>
+                                <img src={men.avatar} alt="" width={50} height={50} className="rounded rounded-circle" />
+                                <div className={`p-3 rounded w-100 mt-3 ${men.usuario === id ? "color-chat1" : "color-chat2"}`}>
+                                    <p className="m-0 text-white fw-bolder">{men.nombre}</p>
+                                    <p className="m-0">{men.contenido}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))
-                ) : (<>Error...</>)
-            }
-            <div ref={divRef}></div>
+                        ))
+                    ) : (<>Error...</>)
+                }
+                <div ref={divRef}></div>
+            </div>
             <form action="" onSubmit={enviarMensaje} className="w-100 px-3">
                 <div className="w-100 bg-white">
                     <input type="text" className="p-3 w-75" placeholder="Escribe tu comentario o pregunta..." ref={mensajeRef} />
                     <button className="btn w-25 btn-primary h-100">Enviar</button>
                 </div>
             </form>
-        </div>
+        </>
     )
 }
