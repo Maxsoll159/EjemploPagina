afc693b4c2c5ee55cf9615d68e845bd0e014d7f1
Martin <mar_1905@hotmail.es>
2022-10-26 16:55:55 -0500

    Facebok

--- src/pages/Seminarios.jsx
+++ src/pages/Seminarios.jsx
@@ -87,7 +87,7 @@ export const Seminarios = () => {
                             {
                                 seminarios.tipo === "PROXIMO" ? (
                                     seminarios.source === "facebook" ? (
-                                        <iframe src="https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2F8filmcine%2Fvideos%2F676706103723348%2F&show_text=false&width=560&t=0" width={900} height={600} className="d-block mx-auto" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+                                        <iframe src={seminarios.id_video} width={900} height={600} className="d-block mx-auto" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
                                     ) : seminarios.source === "normal" ? (
                                         <ReactHlsPlayer
                                             src={`https://antmediaserver.desarrolloglobal.pe:5443/LiveApp/streams/${seminarios.codigo_envivo}.m3u8`}
